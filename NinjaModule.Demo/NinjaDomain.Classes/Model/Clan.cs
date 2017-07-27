8034d77f712e05af50ffa7e876d8a69927fc3f5e
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-07-27 20:54:50 +0200

    Added new model classes.

---
+++ NinjaModule.Demo/NinjaDomain.Classes/Model/Clan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classes.Model
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+
+        public string ClanName { get; set; }
+
+        public List<Ninja> Ninjas { get; set; }
+    }
+}
