11fa8090f42897209604742bc25e60ac7f846ab9
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-08-05 11:37:56 +0200

    Update some filed.

---
+++ LinqExamples/LinqExamples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> listEmployees = new List<Employee>
+            {
+                new Employee() { Name = "Ben", JobTitle = "Developer", City = "London" },
+                new Employee() { Name = "John", JobTitle = "Sr. Developer", City = "Bangalore" },
+                new Employee() { Name = "Steve", JobTitle = "Developer", City = "Bangalore" },
+                new Employee() { Name = "Stuart", JobTitle = "Sr. Developer", City = "London" },
+                new Employee() { Name = "Sara", JobTitle = "Developer", City = "London" },
+                new Employee() { Name = "Pam", JobTitle = "Developer", City = "London" }
+            };
+
+            var employess = listEmployees.ToLookup(x => x.JobTitle);
+
+            Console.WriteLine("Employees Grouped By JobTitle");
+            foreach (var emp in employess)
+            {
+                Console.WriteLine(emp.Key);
+                foreach (var item in emp)
+                {
+                    Console.WriteLine($"{item.Name} -- {item.JobTitle} -- {item.City}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- NinjaModule.Demo/NinjaDomain.Classes/Model/Clan.cs
+++ NinjaModule.Demo/NinjaDomain.Classes/Model/Clan.cs
@@ -11,7 +11,5 @@ namespace NinjaDomain.Classes.Model
         public int Id { get; set; }
 
         public string ClanName { get; set; }
-
-        public List<Ninja> Ninjas { get; set; }
     }
 }
